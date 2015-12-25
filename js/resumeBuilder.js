f524c683a20aa0cbc039d52ea2bde5c77600bf53
Lucky2710 <lakshmi.db96@gmail.com>
2015-12-25 16:38:01 +0530

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- var email = "lucky@yahoo.com";
- var new email = email.replace("yahoo","gmail");
- console.log(email);
- console.log(newemail);
+var email = "lucky@yahoo.com";
+var newemail = email.replace("yahoo","gmail");
+console.log(email);
+console.log(newemail);
  
